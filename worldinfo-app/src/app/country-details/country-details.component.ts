f67e4b856fb2a4102e536fa680e00adab51098b8
MaciekZ23 <maciekz232002@o2.pl>
2025-06-15 16:36:09 +0200

    Rozpoczecie implementacji panelu szczegolow kraju

--- worldinfo-app/src/app/country-details/country-details.component.ts
+++ worldinfo-app/src/app/country-details/country-details.component.ts
@@ -1,14 +1,30 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+export interface Country {
+  name: string;
+  capital: string;
+  flag: string;
+  region: string;
+  population: number;
+  area?: number;
+  currency?: string;
+  languages?: string[];
+  timezone?: string;
+}
+
 @Component({
   selector: 'app-country-details',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './country-details.component.html',
-  styleUrls: ['./country-details.component.scss'],
-  standalone: true,
+  styleUrls: ['./country-details.component.scss']
 })
 export class CountryDetailsComponent {
-  @Input() country: any;
+  @Input() country!: Country;
   @Output() close = new EventEmitter<void>();
+
+  closePanel(): void {
+    this.close.emit();
+  }
 }
